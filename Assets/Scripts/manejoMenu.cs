b6dcc6fbabe823721bf460582d0a1c58606c0823
AndrikSJD <110579197+AndrikSJD@users.noreply.github.com>
2023-01-06 14:58:21 -0600

    MainMenu first commit

---
+++ Assets/Scripts/manejoMenu.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class manejoMenu : MonoBehaviour
+{
+    [SerializeField] AudioSource audioAmbiente;
+   // private Renderer _myRenderer;
+   // private Vector3 _startingPosition;
+
+    public void Start()
+    {
+        //_startingPosition = transform.parent.localPosition;
+        //_myRenderer = GetComponent<Renderer>();
+    }
+
+    public void CargarTour()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void CargarAcercaDe()
+    {
+        SceneManager.LoadScene(1); // Carga escena de acerca de
+    }
+
+    public void CargarMenu()
+    {
+        SceneManager.LoadScene(0); //carga menu
+    }
+
+    public void SalirApp()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+
+}
